fbf7db7110bae82318dac6d9609774870b5a739b
Zak Clark <zak@zakariahclark.com>
2019-03-28 11:30:55 -0700

    Working!

--- assets/app.js
+++ assets/app.js
@@ -14,7 +14,7 @@ var config = {
   $("#addTrainBtn").on("click", function(){
       var trainName = $("#nameInput").val().trim();
       var destination = $("#destinationInput").val().trim();
-      var firstTrain = moment($("#timeInput").val().trim(), "HH:mm" ).subtract(10,"years").format("x"); 
+      var firstTrain = moment($("#timeInput").val().trim(), "HH:mm" ).subtract(10,"years").format("X"); 
       var frequency = $("#frequencyInput").val().trim();
 
       var addNewTrain = {
@@ -26,13 +26,35 @@ var config = {
 
       trainData.ref().push(addNewTrain);
 
-      alert("Success");
+      alert("Your entry was successful.");
 
-    //   Form fields do populate the console
-    //   console.log(trainName);
-    //   console.log(destination);
-    //   console.log(firstTrain);
-    //   console.log(frequency);
-    //   return false;
+      // Form fields do populate the console
+      // console.log(trainName);
+      // console.log(destination);
+      // console.log(firstTrain);
+      // console.log(frequency);
+      // return false;
       
   })
+
+  trainData.ref().on("child_added", function(snapshot){
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
+
+    var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes,"m").format("hh:mm A");
+
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival);
+
+    $("#trainTable > tbody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
+   
+
+  })
+
+
+
